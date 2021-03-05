b82d30061da83a6a6a798ea6b1dfc452c7d578ac
ellnka <alenkasmail@gmail.com>
2021-03-05 17:47:31 +0200

    tiny fix

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,7 +4,7 @@ const Result = ({ result, playAgain }) => {
     return (
         <div className="result">
             <span>{ (result === "win") ? "You win" : (result === "lose") ? "You lose" : "Draw" } </span>
-            <button className="play-again-btn" onClick={() => playAgain()}>Play again</button>
+            <button className="play-again-btn" onClick={() => { playAgain(); } }>Play again</button>
         </div>
     );
 }
